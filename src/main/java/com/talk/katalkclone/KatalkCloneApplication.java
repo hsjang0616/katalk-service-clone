8b07ae4406d27be3f9daf659a5b4627946aa267d
hyosung.ui <hyosungjang.ui@gmail.com>
2025-07-02 18:02:35 +0900

    init setting

---
+++ src/main/java/com/talk/katalkclone/KatalkCloneApplication.java
@@ -0,0 +1,13 @@
+package com.talk.katalkclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatalkCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatalkCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talk/katalkclone/KatalkCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talk.katalkclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatalkCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
